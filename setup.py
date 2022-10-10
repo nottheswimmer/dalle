2da26c7f037f28fb65ff7af1f94258d4edba73ba
Michael Phelps <mphelps@box.com>
2022-10-10 00:07:47 -0400

    Bump to 0.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='pydalle',
-    version='0.1.3',
+    version='0.2.0',
     description='A library for providing programmatic access to the DALL·E 2 API',
     long_description=long_description,
     long_description_content_type='text/markdown',
